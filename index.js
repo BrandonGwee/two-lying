e51c99047042833e1e0f6e4f536ed98bb8b4b5b2
BrandonGwee <BG06.23@ichat.sp.edu.sg>
2024-12-08 15:04:23 +0800

    landing page choose role

--- index.js
+++ index.js
@@ -1,25 +1,60 @@
 const apiUrl = '.'
 
-function clearEverything() {
-    fetch(`${apiUrl}/person`, {
-        method: 'DELETE',
-    })
-    .then(() => {
-        console.log("Cleared person table");
-    })
-    .catch((error) => console.error("Error deleting people:", error));
+function selectRole() {
+    const playerButton = document.getElementById('player');
+    const guesserButton = document.getElementById('guesser');
 
-    fetch(`${apiUrl}/article`, {
-        method: 'DELETE',
-    })
-    .then(() => {
-        console.log("Cleared person table");
-    })
-    .catch((error) => console.error("Error deleting people:", error));
+    let personRole = '';
+
+    // Note: onclick is a property, not a method()
+    playerButton.onclick = () => {
+        personRole = 'player';
+        enterName(personRole);
+    }
+
+    guesserButton.onclick = () => {
+        personRole = 'guesser';
+        enterName(personRole);
+    }
+
+    
+}
+
+function enterName(role) {
+    const newPersonDiv = document.getElementById('newPerson');
+
+    newPersonDiv.innerHTML = null;
+
+    newPersonDiv.innerHTML = 
+        `<p>You can call me:</p>
+            <div class="name">
+                <input id="personName" placeholder="Enter a name">
+                <button type="button" id="submitButton">Enter</button>
+            </div>`;
+            
+
+    const submitButton = document.getElementById('submitButton');
+    const personName = document.getElementById('personName').value;
+
+    submitButton.onclick = () => {
+        fetch(`${apiUrl}/person`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                name: personName,
+                role: role,
+            })
+        })
+        .then((response) => response.json())
+        .then(window.location = `${apiUrl}/gameRoom.html`)
+        .catch((error) => console.error('Error adding task:', error));
+    }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    clearEverything();
+    selectRole();
 
 })
